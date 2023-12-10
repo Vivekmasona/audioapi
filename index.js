47108a43895a04223a94b1d39d51aa2feb21199b
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-10 19:44:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.get('/download', async (req, res) => {
   try {
-    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
-    const title = req.query.title || 'audio'; // Get the title query parameter or use 'audio' as default
+    const videoURL = req.query.url;
 
     if (!videoURL) {
       return res.status(400).send('Missing video URL');
     }
 
-    // Get information about the video (including file size)
     const info = await ytdl.getInfo(videoURL);
-    const fileSize = info.formats[0].contentLength; // Get the file size in bytes
+    const title = info.videoDetails.title.replace(/[^\w\s]/gi, '');
 
-    // Set response headers to specify a downloadable file with the specified title
+    const fileSize = info.formats[0].contentLength;
+
     res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
     res.setHeader('Content-Type', 'audio/mpeg');
-   // res.setHeader('Content-Length', fileSize); // Set the Content-Length header
+    res.setHeader('Content-Length', fileSize);
 
-    // Pipe the video stream into the response
     ytdl(videoURL, { filter: 'audioonly' }).pipe(res);
   } catch (error) {
     console.error('Error:', error);
