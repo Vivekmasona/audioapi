e4ff48db3249e8c7f8db0b01e35b48964f940010
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-10 19:36:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 const app = express();
+const port = 3000;
 
 app.get('/download', async (req, res) => {
-  const ytUrl = req.query.url;
-  if (!ytUrl) {
-    return res.status(400).send('Error: YouTube URL is missing in the query parameter.');
-  }
-
-  const apiUrl = `https://youtube-mp36.p.rapidapi.com/dl?id=${encodeURIComponent(ytUrl)}`;
-  const rapidApiKey = '11939ea42cmsh9be181f6708fc39p162794jsn9e46f87d898b';
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': rapidApiKey,
-      'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com',
-    },
-  };
-
   try {
-    const response = await fetch(apiUrl, options);
-    const result = await response.text();
-    res.send(result);
+    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
+    const title = req.query.title || 'audio'; // Get the title query parameter or use 'audio' as default
+
+    if (!videoURL) {
+      return res.status(400).send('Missing video URL');
+    }
+
+    // Get information about the video (including file size)
+    const info = await ytdl.getInfo(videoURL);
+    const fileSize = info.formats[0].contentLength; // Get the file size in bytes
+
+    // Set response headers to specify a downloadable file with the specified title
+    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    res.setHeader('Content-Type', 'audio/mpeg');
+   // res.setHeader('Content-Length', fileSize); // Set the Content-Length header
+
+    // Pipe the video stream into the response
+    ytdl(videoURL, { filter: 'audioonly' }).pipe(res);
   } catch (error) {
-    console.error(error);
+    console.error('Error:', error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
